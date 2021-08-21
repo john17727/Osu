dba1c873b1510ae120eb7217b7ed34e8fc0d61f4
Juan Rincon <justanotherone17727@gmail.com>
2021-08-21 15:57:25 -0600

    Added bottom navigation

--- app/src/main/java/com/example/osu/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/osu/ui/navigation/Navigation.kt
@@ -1,5 +1,70 @@
 package com.example.osu.ui.navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.osu.ui.Dashboard
+import com.example.osu.ui.Exercises
+import com.example.osu.ui.Profile
+import com.example.osu.ui.Workout
 
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "dashboard") {
+        composable("dashboard") {
+            Dashboard()
+        }
+        composable("workout") {
+            Workout()
+        }
+        composable("exercises") {
+            Exercises()
+        }
+        composable("profile") {
+            Profile()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = 0.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = MaterialTheme.colors.primary,
+                unselectedContentColor = MaterialTheme.colors.onBackground,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(imageVector = item.icon, contentDescription = item.name)
+                        if (selected) {
+                            Text(text = item.name, textAlign = TextAlign.Center)
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
